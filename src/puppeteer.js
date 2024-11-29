022b11acbdea88f36b3b329226ff98d3adb4a449
Fund <fundgao@gmail.com>
2024-11-29 21:54:13 +0800

    feat: add luce page

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,18 +1,30 @@
 import puppeteer from "puppeteer";
-// Or import puppeteer from 'puppeteer-core';
+// Or
+// import puppeteer from "puppeteer-core";
 
 // 随即延迟
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+const AndriodUA =
+  "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36";
+
+const WebUA =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36";
+
+// 涨幅榜
+const URL_zhang_fu = "https://m.10jqka.com.cn/hq/rank/";
+// 行情中心
+const URL_hang_qing = "https://m.10jqka.com.cn/stockpage/hq#aSwipe=1";
+
 const main = async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
 
   // 设置 User-Agent
-  await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
-  );
+  await page.setUserAgent(AndriodUA);
 
   // 禁用 WebGL 和 WebRTC
   await page.evaluate(() => {
@@ -24,36 +36,53 @@ const main = async () => {
     window.confirm = function () {};
   });
 
+  // Navigate the page to a URL.
+  await page.goto(URL_zhang_fu, {
+    waitUntil: "networkidle2", // 等待网络空闲
+    timeout: 60000, // 设置超时时间
+  });
+
+  // Set screen size.
+  await page.setViewport({ width: 430, height: 932 });
+
+  // 点击更多
+  await page.locator(".more_link").click();
+
   // 模拟用户行为
   await delay(Math.random() * 5000 + 2000); // 随机等待 2 到 7 秒
 
-  // 填充表单并提交
-  //   await page.type("#inputField", "Hello World!", { delay: 100 });
-  //   await page.click("#submitButton");
+  // 使用CSS选择器获取涨幅榜信息
+  const stockData = await page.evaluate(() => {
+    // 查找涨幅榜列表元素
+    const promotions = document.querySelectorAll(".marketStocks li");
 
-  // Navigate the page to a URL.
-  await page.goto("https://developer.chrome.com/");
+    // 提取涨幅榜信息
+    const data = [];
+    promotions.forEach((item) => {
+      const textArray = item.innerText.split("\n");
+      const [stock, price, change, volume, turnover_rate] = textArray;
+      data.push({
+        stock,
+        price,
+        change,
+        volume,
+        turnover_rate,
+      });
+    });
+    return data;
+  });
 
-  // Set screen size.
-  await page.setViewport({ width: 1080, height: 1024 });
+  // 输出抓取的数据
+  console.log("涨幅榜信息:");
+  stockData.forEach((stock, index) => {
+    console.log(`  股票名称: ${stock.stock}`);
+    console.log(`  价格: ${stock.price}`);
+    console.log(`  涨跌幅: ${stock.change}`);
+    console.log(`  成交量: ${stock.volume}`);
+    console.log(`  换手率: ${stock.turnover_rate}`);
+  });
 
-  // 截图
-  //   await page.screenshot({ path: "screenshot.png" });
-
-  // Type into search box.
-  await page.locator(".devsite-search-field").fill("automate beyond recorder");
-
-  // Wait and click on first result.
-  await page.locator(".devsite-result-item-link").click();
-
-  // Locate the full title with a unique string.
-  const textSelector = await page
-    .locator("text/Customize and automate")
-    .waitHandle();
-  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-
-  // Print the full title.
-  console.log('The title of this blog post is "%s".', fullTitle);
+  // TODO 上传接口
 
   await browser.close();
 };
